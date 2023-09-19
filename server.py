c9d08231cf8cd3a6848d540302e69fcc28f89e28
Artem Dorofeev <a.dorofeev@ukr.net>
2023-09-20 00:31:06 +0300

    upd server(add cmd - exch, change currency, day)

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import asyncio
 import aiohttp
 import logging
 import names
+from datetime import datetime, timedelta
 import websockets
 from websockets import WebSocketServerProtocol, WebSocketProtocolError
 from websockets.exceptions import ConnectionClosedOK
@@ -9,9 +10,30 @@ from websockets.exceptions import ConnectionClosedOK
 logging.basicConfig(level=logging.INFO)
 
 LINK_BANK = 'https://api.privatbank.ua/p24api/exchange_rates?date='
-date_currency = '18.09.2023'
-currency = "USD"
+# date_currency = '18.09.2023'
+# currency = "USD"
 
+
+def get_date():
+    d = datetime.now()
+    day = str(d.day) if d.day > 9 else '0' + str(d.day)
+    month = str(d.month) if d.month > 9 else '0' + str(d.month)
+    return f"{day}.{month}.{str(d.year)}"
+
+
+def get_list_date(day: int):
+    list_date = []
+    d = datetime.now()
+    for i in range(day):
+        delta_day = timedelta(days=i)
+        new_date = d - delta_day
+        day = str(new_date.day) if new_date.day > 9 else '0' + \
+            str(new_date.day)
+        month = str(new_date.month) if new_date.month > 9 else '0' + \
+            str(new_date.month)
+        date_of_change = day + '.' + month + '.' + str(new_date.year)
+        list_date.append(date_of_change)
+    return list_date
 
 
 async def request(url: str):
@@ -28,17 +50,63 @@ async def request(url: str):
             return None
 
 
-async def get_exchange():
-    # result = await request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
-    result = await request(LINK_BANK + date_currency)
-    if result:
-        temp_info = result['exchangeRate']
-        exc, = list(filter(lambda el: el["currency"] == currency, temp_info))
-        return f'{currency}: buy: {exc["purchaseRate"]}, sale: {exc["saleRate"]}. Date: {date_currency}'
-    return "Failed to retrieve data"
+async def get_exchange(currency, day_cur):
+    list_date = get_list_date(day_cur)
+    result_currency = ''
+
+    for i in list_date:
+        result = await request(LINK_BANK + i)
+        if result:
+            temp_info = result['exchangeRate']
+            exc, = list(
+                filter(lambda el: el["currency"] == currency, temp_info))
+            result_currency += f' {currency}: buy: {exc["purchaseRate"]}, sale: {exc["saleRate"]}. Date: {i} '
+        else:
+            result_currency += f' Failed to retrieve data: {i} '
+
+    return result_currency
+
+
+COMMANDS = {
+    "exchange": ("exchange", "exch", "exc", "обмін", ),
+}
+
+CURRENCY_VAL = {"USD": ("usd", "dollar", "долар", "usa", ),
+                "EUR": ("eur", "євро", "евро", ),
+                "GBP": ("gbp", "фунт", ),
+                "CHF": ("chf", "франк", ),
+                "PLZ": ("plz", "злотий"),
+                }
+
 
 def parser(text: str):
-    pass
+    exch_flag = False
+    exch_cur = "USD"
+    exch_day = 1
+    text = text.lower().strip().split()
+
+    for key, val in CURRENCY_VAL.items():
+        for items in val:
+            if items in text:
+                exch_cur = key
+                text.remove(items)
+
+    for key, val in COMMANDS.items():
+        for items in val:
+            if items in text:
+                exch_flag = True
+                text.remove(items)
+
+    for i in text:
+        try:
+            exch_day = int(i)
+        except ValueError:
+            continue
+
+    if exch_day > 5:
+        exch_day = 5
+
+    return exch_flag, exch_cur, exch_day
 
 
 class Server:
@@ -68,10 +136,12 @@ class Server:
 
     async def distrubute(self, ws: WebSocketServerProtocol):
         async for message in ws:
-            if message.lower().startswith('exc'):
-            # if message == 'exc':
-                 r = await get_exchange()
-                 await self.send_to_clients(r)
+            flag, exch_cur, exch_day = parser(message)
+
+            if flag:
+
+                r = await get_exchange(exch_cur, exch_day)
+                await self.send_to_clients(r)
             else:
                 await self.send_to_clients(f"{ws.name}: {message}")
 
